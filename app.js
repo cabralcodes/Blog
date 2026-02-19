8a84f2274a46e1f1295fec8832a7f37ed2c63685
Matheus <matheusaugustodev.web@gmail.com>
2026-02-19 11:21:53 -0300

    criando a arquitetura do projeto

---
+++ app.js
@@ -0,0 +1,15 @@
+// Carregando módulos
+    import express from "express";
+    import handlebars from "handlebars";
+    import bodyParser from "body-parser";
+    const app = express()
+    //import mongoose from "mongoose";
+// Configurações
+
+// Rotas
+
+// Outros
+const PORT = 49823
+app.listen(PORT, () => {
+    console.log("Servidor rodando!")
+})
