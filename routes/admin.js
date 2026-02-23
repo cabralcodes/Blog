e19fbd40d7d1537557cc19ff0e7e251f846c1d58
Matheus <matheusaugustodev.web@gmail.com>
2026-02-23 17:53:42 -0300

    Criando a validação de formulários

--- routes/admin.js
+++ routes/admin.js
@@ -16,16 +16,40 @@ router.get("/categorias", (req, res) => {
 })
 
 router.post("/categorias/nova", (req, res) =>{
+    
+    var erros = []
+
+    if(!req.body.nome || req.body.nome == undefined || req.body.nome == null){
+        erros.push({texto: "Nome inválido"})
+    }
+
+    if(!req.body.slug || typeof req.body.slug == undefined || req.body.slug == null){
+        erros.push({texto: "Slug Inválido"})
+    }
+    
+    if(req.body.nome.length < 2){
+        erros.push({texto: "Nome da categoria é muito pequeno"})
+    }
+
+    if(erros.length > 0){
+        res.render("admin/addcategorias", {erros: erros})
+    }
+    else{
+    
+    
     const novaCategoria = {
         nome: req.body.nome,
         slug: req.body.slug
     }
 
     new Categoria(novaCategoria).save().then(() =>{
-        console.log("Categoria salva com sucesso!")
+        req.flash("success_msg", "Categoria Criada com Sucesso!")
+        res.redirect("/admin/categorias")
     }).catch((err)=>{
-        console.log("Erro ao salvar Categoria: ")
+        req.flash("error_msg", "Houve um erro ao salvar a categoria, tente novamente!")
+        res.redirect("/admin")
     })
+}
 })
 
 router.get("/categorias/add", (req,res) => {
