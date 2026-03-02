1b4e4987e84d8967674f650c419083bdaf73f4e6
Matheus <matheusaugustodev.web@gmail.com>
2026-03-02 16:39:30 -0300

    criação de formulário de postagens

--- routes/admin.js
+++ routes/admin.js
@@ -81,4 +81,17 @@ router.get("/categorias/add", (req,res) => {
     res.render("admin/addcategorias")
 })
 
+router.get("/postagens", (req,res) =>{
+    res.render("admin/postagens")
+})
+
+router.get("/postagens/add", (req,res)=> {
+    Categoria.find().lean().then((categorias) => {
+        res.render("admin/addpostagem", {categorias: categorias})
+    }).catch((err) => {
+        req.flash("error_msg", "Houve um erro ao carregar o formulário")
+        res.redirect("/admin")
+    })
+})
+
 export default router;
